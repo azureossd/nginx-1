9bda7fc846073b7ec8003f413b8d30053a62fe60
Kendrick <Kendubu1@users.noreply.github.com>
2021-01-25 19:40:09 -0600

    Update nginxlab1.sh

--- nginxlab1.sh
+++ nginxlab1.sh
@@ -15,7 +15,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - 
 
-sudo apt install -y nodej
+sudo apt install -y nodejs
 sudo apt install npm -y
 sudo apt install yarn -y
 sudo npm install -g pm2 -y
